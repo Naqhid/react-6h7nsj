8bfc6335797efb4cd16e75d1dc22482dd4ba06d8
Naqhid <mnaqhid@gmail.com>
2019-10-08 21:59:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-6h7nsj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-6h7nsj)
---
+++ config.js
@@ -0,0 +1,5 @@
+export default {
+  appId: "92234c71669e40", //Enter your App ID
+  apiKey: "2b42bda07bb6b2ad2ef697682dcd65ab17ba231b", //Enter your API KEY
+  GUID: "testgroup", // Enter your group UID
+};
